54851c5cce5b893953333ae572b2cc586764ead1
rockroman <rock.rakic@gmail.com>
2022-11-02 16:10:15 +0000

    Make the test fail(red light) again

--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -5,6 +5,10 @@ describe("Calculator",() => {
         test("should return 42 for 20 + 22", () =>{
             expect(addition(20, 22)).toBe(42);
         })
+        test("should return 17 for 12 + 5", () =>{
+            expect(addition(12, 5)).toBe(17);
+        })
+
 
     });
     describe("Subtraction function", () =>{
