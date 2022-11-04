c11ac536d876b0913a3e225c61e9fb747321f9c3
rockroman <rock.rakic@gmail.com>
2022-11-04 00:17:23 +0000

    Write test for entire html file using mocking and separate tet for h1 element

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -1,13 +1,23 @@
+/**
+ * @jest-environment jsdom
+ */
 const { TestScheduler } = require("jest");
 const buttonClick= require("../button")
 
 beforeEach(() => {
-    document.body.innerHTML ="<p id='par'></p>"
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html","utf-8");
+    document.write(fileContents);
+    document.close();
+    
 });
 
 describe("DOM testing", () =>{
     test("expext p content to be certain string",()=>{
         buttonClick();
         expect(document.getElementById("par").innerHTML).toEqual("you clicked")
+    });
+    test("h1 should exist", () =>{
+        expect(document.getElementsByTagName("h1").length).toBe(1)
     });
 });
